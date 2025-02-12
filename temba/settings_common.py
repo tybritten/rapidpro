7735e93bc3acc11781d6dca1a87ad2118cb4bd74
Rowan Seymour <rowanseymour@gmail.com>
2025-02-12 17:03:17 -0500

    Revert "Remove no longer needed custom auth backend"
    This reverts commit b9bf812b6cc351cfc0ee8f4990bd71ea872bae07.

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -613,6 +613,8 @@ API_PERMISSIONS = {
 LOGIN_URL = "/users/login/"
 LOGIN_REDIRECT_URL = "/org/choose/"
 
+AUTHENTICATION_BACKENDS = ("temba.orgs.backend.AuthenticationBackend",)
+
 AUTH_USER_MODEL = "users.User"
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 8}},
